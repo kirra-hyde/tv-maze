df2cdea80fcd65dfb609092b8ebfec6d4bb1b13f
Igor Oganesian <igor.oganesian@gmail.com>
2023-09-04 17:55:58 -0400

    finished part 1

--- tvmaze.ts
+++ tvmaze.ts
@@ -4,9 +4,36 @@ import jQuery from 'jquery';
 const $ = jQuery;
 
 const $showsList = $("#showsList");
+const $showsButton = $(".Show-getEpisodes")
 const $episodesArea = $("#episodesArea");
+const $episodesList = $("#episodesList");
 const $searchForm = $("#searchForm");
 
+const BASE_URL = "https://api.tvmaze.com/";
+const MISSING_URL = "https://tinyurl.com/tv-missing";
+
+interface ShowInterface {
+  id: number,
+  name: string,
+  summary: string,
+  image: string,
+}
+
+interface ShowResultInterface {
+  show: {
+    id: number,
+    name: string,
+    summary: string,
+    image: { medium: string; } | null,
+  };
+}
+
+interface EpisodeInterface {
+  id: number,
+  name: string,
+  season: number,
+  number: number,
+}
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -16,32 +43,33 @@ const $searchForm = $("#searchForm");
  */
 
 //https://api.tvmaze.com/search/shows?q=girls
-const BASE_URL = "https://api.tvmaze.com/search/shows/"
+async function searchShowsByTerm(term: string): Promise<ShowInterface[]> {
+  const response = await axios.get(`${BASE_URL}search/shows/?q=${term}`);
 
-async function searchShowsByTerm(term: string): Promise<object[]> {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-  const results = await axios.get(`BASE_URL?q=${term}`);
-  const shows: object[] = results.data.map(show => {
-                                          show.id,
-                                          show.name,
-                                          show.summary,
-                                          show.image
-                                        });
-  return shows;
+  const result: ShowResultInterface[] = response.data;
+  return result.map(result => {
+    const s = result.show;
+    return {
+      id: s.id,
+      name: s.name,
+      summary: s.summary,
+      image: s.image?.medium || MISSING_URL,
+    };
+  });
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows: object[]): void {
+function populateShows(shows: ShowInterface[]): void {
   $showsList.empty();
 
-  for (let show of shows) {
+  for (const show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src=${show.image?.medium || "https://tinyurl.com/tv-missing"}
+              src=${show.image}
               alt=${show.name}
               class="w-25 me-3">
            <div class="media-body">
@@ -55,7 +83,8 @@ function populateShows(shows: object[]): void {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
@@ -63,15 +92,15 @@ function populateShows(shows: object[]): void {
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
 
-async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
+async function searchForShowAndDisplay(): Promise<void> {
+  const term = $("#searchForm-term").val() as string;
   const shows = await searchShowsByTerm(term);
 
   $episodesArea.hide();
   populateShows(shows);
 }
 
-$searchForm.on("submit", async function (evt) {
+$searchForm.on("submit", async function (evt: JQuery.SubmitEvent) {
   evt.preventDefault();
   await searchForShowAndDisplay();
 });
@@ -81,8 +110,44 @@ $searchForm.on("submit", async function (evt) {
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id: number): Promise<EpisodeInterface[]> {
+  const response = await axios.get(`${BASE_URL}episodes/${id}`);
 
-/** Write a clear docstring for this function... */
+  const result: EpisodeInterface[] = response.data;
+  return result.map(e => ({
+      id: e.id,
+      name: e.name,
+      season: e.season,
+      number: e.number,
+  }));
+}
 
-// function populateEpisodes(episodes) { }
+/** Given list of episodes, create markup for each and to DOM */
+
+function populateEpisodes(episodes: EpisodeInterface[]): void {
+  $episodesList.empty();
+
+  for (const episode of episodes) {
+    const $listItem = $(
+      `<li>
+        ${episode.name}
+        Season: ${episode.season}
+        Ep #${episode.number}
+      </li>`
+      );
+
+    $episodesList.append($listItem);
+  }
+  $episodesArea.show();
+ }
+
+/**  */
+
+ async function getAndDisplayEpisodes(evt: JQuery.ClickEvent) : Promise<void> {
+  const showId = Number(evt.target().closest(".Show").attr("data-show-id"));
+
+  const episodes = await getEpisodesOfShow(showId);
+  populateEpisodes(episodes);
+ }
+
+ $showsButton.on("click", getAndDisplayEpisodes);
